f76ee4b72ebadfaf76cceeb6b6d4d9afcf081806
Twizerimana <gedeontwizerimana6@gmail.com>
2025-09-05 01:25:03 +0300

    setting

--- app/(tabs)/Setting.tsx
+++ app/(tabs)/Setting.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { SettingCard } from "@/components/__ui__/SetingCard";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import back from "../../assets/icon/back.png";
+
 
 export default function TabTwoScreen() {
+  const router = useRouter()
   return (
-    <View className=" h-full flex flex-col justify-center items-center ">
-       <Text>Setting</Text>
+    <View className=" h-full py-10 px-4">
+      <TouchableOpacity className="py-5 flex flex-row justify-between items-center gap-1 w-2/3 pr-8" onPress={() => router.back()}>
+         <Image source={back} className="w-10 h-10" />
+          <Text className="text-lg font-bold">Settings</Text>
+      </TouchableOpacity>
+      <View>
+         <SettingCard name="person-outline" title="Profile" icon="arrow-back"/>
+      </View>
+      <View className="flex flex-col justify-center items-center">
+       <TouchableOpacity className="flex flex-row justify-center items-center bg-slate-200 w-2/3 rounded-3xl py-2">
+         <MaterialIcons name="logout" size={24} color='red'/>
+         <Text className="text-red-500">Logout</Text>
+       </TouchableOpacity>
+       </View>
      </View>
   );
 }
---
+++ components/__ui__/SetingCard.tsx
@@ -0,0 +1,23 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+
+type MaterialIconName = React.ComponentProps<typeof MaterialIcons>["name"];
+
+interface IProps {
+  name: MaterialIconName;
+  title: string;
+  icon:MaterialIconName
+}
+
+
+export const SettingCard = (props:IProps) => {
+    return (
+      <View className="flex flex-row justify-between items-center border-b border-slate-200">
+         <View className="flex flex-row justify-center items-center gap-2">
+            <MaterialIcons name={props.name} size={24} color='#0EA5E9'/>
+            <Text></Text>
+         </View>
+         <MaterialIcons name={props.icon} size={16} color='gray'/>
+      </View>
+    )
+}
